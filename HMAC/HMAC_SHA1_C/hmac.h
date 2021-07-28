84ffa6c2d3ed37c8981960304e2a9d62f6619e05
Raghvender <raghvender1205@gmail.com>
2021-07-29 02:41:57 +0530

    HMAC SHA-1 C Implementation

---
+++ HMAC/HMAC_SHA1_C/hmac.h
@@ -0,0 +1,13 @@
+#ifndef HMAC_H
+#define HMAC_H_ (1)
+
+#include <stdint.h>
+
+void hmac_sha1(const uint8_t *k,   /* secret key */
+        size_t lk,  /* length of the key in bytes */
+        const uint8_t *d,   /* data */
+        size_t ld,  /* length of data in bytes */
+        uint8_t *out, /* output buffer, at least "t" bytes */
+        size_t *t);
+
+#endif // HMAC_H_
